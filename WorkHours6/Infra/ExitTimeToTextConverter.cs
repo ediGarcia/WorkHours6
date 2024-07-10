cfe234115de2bdb79b2c264d89ede9a7d8dd3216
avaz <alex.vaz@capgemini.com>
2024-07-10 19:07:30 -0300

    Fixed error rounding exit time.

--- WorkHours6/Infra/ExitTimeToTextConverter.cs
+++ WorkHours6/Infra/ExitTimeToTextConverter.cs
@@ -1,28 +1,19 @@
-﻿using System;
+﻿using HelperMethods;
+using System;
 using System.Globalization;
-using System.Linq;
 using System.Windows.Data;
 
 namespace WorkHours6.Infra;
 
-public class ExitTimeToTextConverter : IMultiValueConverter
+public class ExitTimeToTextConverter : IValueConverter
 {
     #region Convert
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-    {
-        if (!values.Any() || values[0] is not DateTime || values[1] is not TimeSpan)
-            return "Exit time: --:--";
-
-        DateTime expectedExitTime = (DateTime)values[0];
-        TimeSpan extraHours = (TimeSpan)values[1];
-
-        return
-            $"Exit time: {expectedExitTime:HH:mm}{(extraHours != TimeSpan.Zero ? $" ({(extraHours > TimeSpan.Zero ? "+" : "-")}{Math.Abs((int)extraHours.TotalHours):00}:{Math.Abs(extraHours.Minutes):00})" : "")}";
-    }
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is DateTime exitTime ? DateTimeMethods.RoundToMinutes(exitTime).ToString("HH:mm") : "--:--";
     #endregion
 
     #region ConvertBack
-    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+    public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture) =>
         throw new NotImplementedException();
     #endregion
 }
